62cb2b37b57b8abd91cc06e16f634cc8e18a1916
icsusb <imecsalex@gmx.de>
2022-10-05 10:23:03 +0200

    Basic Push

--- UPLOADpython - WORKSPACE.bat
+++
@@ -1,5 +0,0 @@
-#for B2 Server
-scp -r -i C:\Users\imecs\.ssh\ccuser C:\Users\imecs\Documents\_Workspace\* user15@cctrainer.ddnss.de:/home/user15/_Workspace/
-
-#for Kali
-scp -r -i C:\Users\imecs\.ssh\kaliuser C:\Users\imecs\Documents\_Workspace\* kali@192.168.178.32:/home/kali/_Workspace/
--- UPLOADpython.bat
+++
@@ -1,5 +0,0 @@
-#for B2 Server
-scp -r -i C:\Users\imecs\.ssh\ccuser C:\Users\imecs\Documents\_Workspace\HalloWelt\* user15@cctrainer.ddnss.de:/home/user15/_Workspace/HalloWelt/
-
-#for Kali
-scp -r -i C:\Users\imecs\.ssh\kaliuser C:\Users\imecs\Documents\_Workspace\HalloWelt\* kali@192.168.178.32:/home/kali/_Workspace/HalloWelt/
