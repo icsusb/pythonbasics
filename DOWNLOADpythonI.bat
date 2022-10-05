b30b93348837c414314f1af27363d1d34f862e73
icsusb <imecsalex@gmx.de>
2022-10-05 10:23:03 +0200

    Basic Push

--- DOWNLOADpythonI.bat
+++
@@ -1,8 +0,0 @@
-#for B2 to Windows
-scp -r -i C:\Users\imecs\.ssh\ccuser user15@cctrainer.ddnss.de:/home/user15/_Workspace/HalloWelt/* C:\Users\imecs\Documents\_Workspace\HalloWelt\
-
-#for Kali to Windows
-scp -r -i C:\Users\imecs\.ssh\kaliuser kali@192.168.178.32:/home/kali/_Workspace/HalloWelt/* C:\Users\imecs\Documents\_Workspace\HalloWelt\ 
-
-
-
