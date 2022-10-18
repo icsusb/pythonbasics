dc50d52e509b86412d6cd4a2143be3662c0e109d
icsusb <imecsalex@gmx.de>
2022-10-18 15:16:15 +0200

    Basic Push

---
+++ Milestone10/schmeichelprogramm.py
@@ -0,0 +1,5 @@
+import random
+
+Adjektiv = ['beste', 'liebenswürdigste', 'schönste', 'gröste']
+Norm = ['Mensch', 'Hecht', 'Freund', 'Kumpel', 'wannabe Programierer']
+print("Du bist der" + " " + (random.choice(Adjektiv)) + " " + (random.choice(Norm)))
