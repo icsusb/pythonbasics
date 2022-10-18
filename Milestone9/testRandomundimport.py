74578f8b6b34e399fccc4171afd07b0e4997490b
icsusb <imecsalex@gmx.de>
2022-10-18 15:16:15 +0200

    Basic Push

---
+++ Milestone9/testRandomundimport.py
@@ -0,0 +1,18 @@
+
+
+#import random
+#print(help(random.random))
+#help(random.random)
+
+#import random
+#def zufallszahl():
+#    return random.random() * 9 + 1
+#print (zufallszahl())
+
+
+#import random
+#print(random.random())
+
+import random
+handgeste = ['Schere', 'Stein', 'Papier', 'Echse', 'Spock']
+print(random.choice(handgeste))
