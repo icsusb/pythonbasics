71c2383a049814b497442ad8bc38462da79ed396
icsusb <imecsalex@gmx.de>
2022-10-18 12:39:11 +0200

    Basic Push

---
+++ Milestone9/testIf.py
@@ -0,0 +1,15 @@
+#wert = 3
+#if wert < 5 :
+#    print('Wert ist kleiner als 5')
+#    print('Ich gehöre auch noch zu der Bedingung')
+
+#print('und hier geht es bach der if-Abfrage weiter')
+
+
+wert = 9
+if wert < 5:
+    print('Wert ist kleiner als 5')
+elif wert == 8:
+    print('Wert ist exakt 5')
+else:
+    print('Wert ist größer als 5')
---
+++ Milestone9/testiBoolsche.py
@@ -0,0 +1,8 @@
+
+
+wert_1 = 23
+
+if not wert_1:
+    print('wert_1 ist False')
+else:
+    print('wert_1 ist True')
