2aff4ac0fa73c94ad221daeece361eea54bde8aa
icsusb <imecsalex@gmx.de>
2022-10-19 11:13:45 +0200

    Basic Push

---
+++ Milestone10/lottoziehung.py
@@ -0,0 +1,14 @@
+#Gruppe  Alex ; Axel ; Ralf ; Steffen.
+# Lottoziehung 1 von 49
+# Eine Zufahlszahl von 1 bis 49 generieren.
+
+import random
+
+randomlist = random.sample(range(1, 49), 6)
+reihenfolge = sorted(randomlist)
+print(reihenfolge)
+
+for i in reihenfolge:
+    print (i)
+
+
